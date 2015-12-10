c75daacb82e9ea201d8512b7a3b2318246b58f3c
Aybuke Ozdemir <aybuke.147@gmail.com>
2015-12-10 02:57:09 +0200

    kayıt işlemi gerçekleştirildi #1

--- app/src/main/java/com/example/aybuke/qrcard/Registeration.java
+++ app/src/main/java/com/example/aybuke/qrcard/Registeration.java
@@ -1,11 +1,18 @@
 package com.example.aybuke.qrcard;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import java.util.Random;
 
 public class Registeration extends AppCompatActivity {
 
@@ -25,5 +32,30 @@ public class Registeration extends AppCompatActivity {
             }
         });
     }
+
+    public void KayıtOlDB(View v){
+        EditText adi = (EditText) findViewById(R.id.editText4);
+        EditText mail = (EditText) findViewById(R.id.editText5);
+        EditText parola = (EditText) findViewById(R.id.editText6);
+        CheckBox vip = (CheckBox) findViewById(R.id.checkBox);
+
+        CardDB kayit = new CardDB(getApplicationContext());
+        SQLiteDatabase veritabani = kayit.getWritableDatabase();
+        ContentValues satir = new ContentValues();
+
+        satir.put("name", adi.getText().toString());
+        satir.put("mail", mail.getText().toString());
+        satir.put("parola", parola.getText().toString());
+
+        if (vip.isChecked()) {
+            Random r = new Random();
+            satir.put("vip", r.nextInt(90000) + 10000);
+            }
+
+        veritabani.insert("cards", null, satir);
+        Intent main = new Intent(this, MainActivity.class);
+        startActivity(main);
+        }
+
 
 }
