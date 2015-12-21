b6916246931f617d73fa20b1bf773521ce359d05
Aybuke Ozdemir <aybuke.147@gmail.com>
2015-12-21 02:30:53 +0200

    QRCODE oluşturma methodu eklendi. #7 #8

--- app/src/main/java/com/example/aybuke/qrcard/qr_create.java
+++ app/src/main/java/com/example/aybuke/qrcard/qr_create.java
@@ -6,6 +6,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+
+import org.json.JSONArray;
 
 public class qr_create extends AppCompatActivity {
 
@@ -24,6 +29,25 @@ public class qr_create extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void qrcodeolustur(View v){
+        EditText adres1 = (EditText) findViewById(R.id.editText7);
+        EditText adres2 = (EditText) findViewById(R.id.editText8);
+        EditText adres3 = (EditText) findViewById(R.id.editText9);
+        EditText adres4 = (EditText) findViewById(R.id.editText10);
+        EditText adres5 = (EditText) findViewById(R.id.editText11);
+
+        JSONArray json = new JSONArray();
+        json.put(adres1.getText().toString());
+        json.put(adres2.getText().toString());
+        json.put(adres3.getText().toString());
+        json.put(adres4.getText().toString());
+        json.put(adres5.getText().toString());
+
+        new IntentIntegrator(this).shareText(json.toString());
+
+
     }
 
 }
