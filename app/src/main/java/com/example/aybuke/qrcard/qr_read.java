eba090c84009835271e5ee2e15c8387ba05abff4
irem <iremsendur1@gmail.com>
2015-12-22 00:42:58 +0200

    Qrcode oluşturma özelliği düzenlendi.  #6 #7

--- app/src/main/java/com/example/aybuke/qrcard/qr_read.java
+++ app/src/main/java/com/example/aybuke/qrcard/qr_read.java
@@ -9,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 public class qr_read extends AppCompatActivity {
 
     @Override
@@ -17,8 +20,22 @@ public class qr_read extends AppCompatActivity {
         setContentView(R.layout.activity_qr_read);
         Intent read = getIntent();
         String kod =  read.getStringExtra("okunan");
-        TextView okunan = (TextView) findViewById(R.id.okunan);
-        okunan.setText(kod);
+        JSONArray json = null;
+        try {
+            json = new JSONArray(kod);
+            TextView adres1 = (TextView) findViewById(R.id.textView2);
+            adres1.setText(json.getString(0));
+            TextView adres2 = (TextView) findViewById(R.id.textView3);
+            adres2.setText(json.getString(1));
+            TextView adres3 = (TextView) findViewById(R.id.textView4);
+            adres3.setText(json.getString(2));
+            TextView adres4 = (TextView) findViewById(R.id.textView5);
+            adres4.setText(json.getString(3));
+            TextView adres5 = (TextView) findViewById(R.id.textView6);
+            adres5.setText(json.getString(4));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
